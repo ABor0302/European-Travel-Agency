88276227b9975157d52ae6279bda0b8f2f6a2946
ABor0302 <borhan416@gmail.com>
2024-06-13 15:11:13 +0300

    Project firs deployed

---
+++ script.js
@@ -0,0 +1,24 @@
+function getWeather() {
+    const apiKey = 'f18a9032ca73caa35d387137f269c79b';
+    const city = document.getElementById('citySelect').value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                <h2>Weather in ${data.name}</h2>
+                <p>Temperature: ${data.main.temp} °C</p>
+                <p>Description: ${data.weather[0].description}</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+            `;
+        })
+        .catch(error => {
+            console.log('Error fetching weather data:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
+        });
+}
